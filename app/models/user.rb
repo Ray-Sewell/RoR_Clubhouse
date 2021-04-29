aa33bff6b60d0936bb32611bf7a14714aed73443
Ray <sewellray11+git@googlemail.com>
2021-04-29 19:37:39 +0100

    migrated name to users, created registration controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts, dependent: :destroy
+
+  validates :email, presence: true, length: { minimum: 4, maximum: 30 }
+  validates :name, presence: true, length: { minimum: 3, maximum: 22 }
 end
